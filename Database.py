e843951e0f0c95df935fd907687e4987888111cf
akop2005 <akop2005@list.ru>
2021-12-14 20:06:49 +0300

    new db

--- Database.py
+++ Database.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app=Flask(__name__)
+app.debug=True
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///second.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db=SQLAlchemy(app)
