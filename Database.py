8802a81d05b1ad91c5918387de87b645dca9d60d
akop2005 <akop2005@list.ru>
2021-12-14 14:50:01 +0300

    go

--- Database.py
+++ Database.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app=Flask(__name__)
-app.config['SQLALCHEM Y_DATABASE_URI']='sqlite:///second.db'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///second.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db=SQLAlchemy(app)
 class Clients(db.Model):
     id=db.Column(db.Integer,primary_key=True)
